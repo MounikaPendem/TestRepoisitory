fdc6999f0e706ae4340c7f8c15ba7c4288ad5667
MounikaPendem <pendemmounika17@gmail.com>
2022-03-14 16:47:28 +0530

    username changed

--- inetBanking-master/src/test/java/com/inetBanking/qa/Testcases/AddCustomerTest.java
+++ inetBanking-master/src/test/java/com/inetBanking/qa/Testcases/AddCustomerTest.java
@@ -14,7 +14,7 @@ public class AddCustomerTest extends TestBase {
 	@Test
 	public void addNewCustomer() throws InterruptedException, IOException {
 		LoginPage login = new LoginPage(driver);
-		login.SetUserName(UserName);
+		login.SetUserName(UserNamesssssssssss);
 		login.SetPassword(Password);
 		login.ClickSubmitBtn();
 		logger.info("Login Successfull");
